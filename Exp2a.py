3456ed62cf0d85d8074a21e1e5c0a25e71501130
KARAN SINGH RATHORE <160389466+karansrabcd01@users.noreply.github.com>
2025-12-26 21:48:18 +0530

    Add files via upload

---
+++ Exp2a.py
@@ -0,0 +1,39 @@
+# Implementing AND logic gate using Perceptron
+import numpy as np
+
+class Perceptron:
+    def __init__(self, learning_rate=0.1, n_iters=10):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def _activation(self, x):
+        return np.where(x >= 0, 1, 0)
+    
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                linear_output = np.dot(x_i, self.weights) + self.bias
+                y_predicted = self._activation(linear_output)
+
+                update = self.lr * (y[idx] - y_predicted)
+                self.weights += update * x_i
+                self.bias += update
+
+    def predict(self, X):
+        linear_output = np.dot(X, self.weights) + self.bias
+        return self._activation(linear_output)
+    
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([0, 0, 0, 1])
+
+model = Perceptron()
+model.fit(X, y)
+predictions = model.predict(X)
+print("For AND Gate :- ")
+print("Predictions:", predictions)
